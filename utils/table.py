ddcf70da2b5ad8d912671add9702ddc4955d649d
MiroFronhoffs <miro.fronhoffs@icloud.com>
2024-10-24 11:04:29 +0200

    all  the basics of table.py made except the function assign_name

---
+++ utils/table.py
@@ -0,0 +1,36 @@
+class Seat:
+    def __init__(self, free, occupant =""):
+        self.free = free
+        self.occupant = occupant
+
+    def set_occupant(self, name):
+        if self.free:
+            self.free = False
+            self.occupant = name
+
+    def remove_occupant(self):
+        previous_occupant = self.occupant
+        self.free  = True
+        self.occupant = ""
+        return previous_occupant
+
+class Table:
+    def __init__(self, seats):
+        self.capacity = len(seats)
+        self.seats = seats
+
+    def has_free_spot(self):
+        for x in self.seats:
+            if x.free:
+                return True
+
+
+    def assign_name(self, name):
+
+
+    def left_capacity(self):
+        capacity_left = self.capacity
+        for x in self.seats:
+            if not x.free:
+                capacity_left -= 1
+        return capacity_left
