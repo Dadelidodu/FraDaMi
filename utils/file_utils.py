aa0ca3aaa3edcc1e6b089d7deb3ec2d911154a49
MiroFronhoffs <miro.fronhoffs@icloud.com>
2024-10-25 10:51:23 +0200

    main.py can now find bouman_8_members.xlsx without moving the file

--- utils/file_utils.py
+++ utils/file_utils.py
@@ -1,7 +1,7 @@
 # Importing pandas to extract data from xslx file
 
 import pandas as pd
-df = pd.read_excel('bouman_8_members.xlsx')
+df = pd.read_excel('../FraDaMi/utils/bouman_8_members.xlsx')
 print(df["Names"])
 
 name_list = (df["Names"]).tolist()
