51bd7473cffb023f933e5520fbcb60691bb798d1
Dadelidodu <anselotdavid@gmail.com>
2024-10-25 15:43:43 +0200

    Update file_utils.py

--- utils/file_utils.py
+++ utils/file_utils.py
@@ -1,6 +1,6 @@
 # Importing pandas to extract data from xslx file
 
 import pandas as pd
-df = pd.read_excel('../FraDaMi/utils/bouman_8_members.xlsx')
+df = pd.read_excel('utils/bouman_8_members.xlsx')
 
 name_list = (df["Names"]).tolist()
