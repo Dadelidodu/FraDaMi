c2dd645e7933aae897075e10bd5621b344d2d6e1
MiroFronhoffs <miro.fronhoffs@icloud.com>
2024-10-25 11:22:31 +0200

    For David and Frank

---
+++ main.py
@@ -0,0 +1,7 @@
+import utils.file_utils
+from utils.table import Table, Seat
+from utils.openspace import Openspace
+
+op = Openspace()
+op.organize()
+op.display()
--- utils/file_utils.py
+++ utils/file_utils.py
@@ -2,7 +2,5 @@
 
 import pandas as pd
 df = pd.read_excel('../FraDaMi/utils/bouman_8_members.xlsx')
-print(df["Names"])
 
 name_list = (df["Names"]).tolist()
-print(name_list)
